dcad8978353a5e259a467c1022a1e4a0424f5368
manro026 <47592702+manro026@users.noreply.github.com>
2020-04-30 13:51:29 +0300

    Обьеденил слои время и календарь, сделал их вместе и 1 кнопкой

--- FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningSetPlane.java
+++ FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningSetPlane.java
@@ -55,55 +55,8 @@ public class TreningSetPlane extends Activity {
 
     }
 
-public void testBut_Click(View v)
-{
-    AlertDialog.Builder builder= new AlertDialog.Builder(TreningSetPlane.this);//создаем и обьявляем билдер
-    final View view = getLayoutInflater().inflate(R.layout.test, null);//нужен для того чтобы искать элементы на диалоговом окне. ТК через билдер не получается
-    builder.setView(view);//загружаем слой который будет отображаться
-    final AlertDialog dlg = builder.create();//нужно для уничтожение обьекта
-    dlg.show();//показ
 
-
-    CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendarView);//слушатель для календаря
-    calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-
-        @Override
-        public void onSelectedDayChange(CalendarView view, int year,
-                                        int month, int dayOfMonth) {
-            mYear = year;
-            mMonth = month;
-            mDay = dayOfMonth;
-        }
-    });
-
-    Button btSet = (Button) view.findViewById(R.id.btSet);
-    btSet.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            /*ain.info_ed.putInt("test",mDay);
-            main.info_ed.commit();
-*/
-            SQLiteDatabase database = dbHelper.getWritableDatabase();//нужен для управления
-            ContentValues contentValues = new ContentValues();//нужен для добавления новых строк в таблицу выглядит как массив с именами столбцов и тд
-
-            String selectedDate = new StringBuilder().append(mMonth + 1)
-                    .append("-").append(mDay).append("-").append(mYear)
-                    .append(" ").toString();//создаем стринг со всеми значениями даты
-            //selectedDate = sdf.format(d); //преобразуем его под нужные нам значения даты
-
-            contentValues.put(DBHelper.KEY_DATATIME, selectedDate);
-            database.insert(DBHelper.TABLE_TRENING, null, contentValues);//записываем в какую таблицу.
-            Toast.makeText(getApplicationContext(), selectedDate+"Данные записаны", Toast.LENGTH_LONG).show();//таск это всплывающие окно, выводим в него наш выбор, заданным форматом
-
-            dbHelper.close();//отключаемся от бд
-            dlg.dismiss();//дестрой окна
-        }
-    });
-
-
-}
-
-public void TimeButton_Click(View v)
+public void DataTimeButton_Click(View v)//суда необходимо добавить проверку на то что если пользователь не выбрал ни дату ни время!
 {
     AlertDialog.Builder builder= new AlertDialog.Builder(TreningSetPlane.this);//создаем и обьявляем билдер
     final View view = getLayoutInflater().inflate(R.layout.clock, null);//нужен для того чтобы искать элементы на диалоговом окне. ТК через билдер не получается
@@ -112,17 +65,23 @@ public void TimeButton_Click(View v)
 
     dlg.show();//показ
 
-    TimePicker timePicker = (TimePicker) view.findViewById(R.id.timePicker);
+    TimePicker timePicker = (TimePicker) view.findViewById(R.id.timePicker);//cлушатель для времени
     timePicker.setIs24HourView(true);
 
     timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
         @Override
         public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-            mHour=hourOfDay;
-            mMinute=minute;
-        }
-    });
+            mHour=hourOfDay;  mMinute=minute;
+        }    });
 
+    CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendarView);//слушатель для календаря
+    calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+        @Override
+        public void onSelectedDayChange(CalendarView view, int year,
+                                        int month, int dayOfMonth) {
+            mYear = year;  mMonth = month; mDay = dayOfMonth;
+        }    });
 
     Button button=(Button) view.findViewById(R.id.button9);
     button.setOnClickListener(new View.OnClickListener() {
