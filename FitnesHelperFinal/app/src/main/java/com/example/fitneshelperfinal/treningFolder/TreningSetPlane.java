9cf69617bfb5ba536aca6f942075086f66f2cb2c
manro026 <47592702+manro026@users.noreply.github.com>
2020-05-01 13:59:30 +0300

    Появилась кнопка сохранить, рабочая, появилось поле для ввода название тренировки. Едем дальше

--- FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningSetPlane.java
+++ FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningSetPlane.java
@@ -48,8 +48,6 @@ public class TreningSetPlane extends Activity {
 
     private String selectedDate;
 
-
-
     private List<View> allEds;//лист для слоев который мы создаем в лайнере
 
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");//модификатор чтобы по нему делать нужную форму даты
@@ -82,7 +80,6 @@ public void DataTimeButton_Click(View v)//суда необходимо доба
     builder.setView(view);//загружаем слой который будет отображаться
     final AlertDialog dlg = builder.create();//нужно для уничтожение обьекта
 
-
     dlg.show();//показ
 
     TimePicker timePicker = (TimePicker) view.findViewById(R.id.timePicker);//cлушатель для времени
@@ -107,24 +104,15 @@ public void DataTimeButton_Click(View v)//суда необходимо доба
     button.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            SQLiteDatabase database = dbHelper.getWritableDatabase();//нужен для управления
-            ContentValues contentValues = new ContentValues();//нужен для добавления новых строк в таблицу выглядит как массив с именами столбцов и тд
-
              selectedDate = new StringBuilder().append(mDay)
                     .append("/").append(mMonth+1).append("/").append(mYear)
                     .append(" ").append(mHour).append(":").append(mMinute).toString();//создаем стринг со всеми значениями даты
-          //  selectedDate = sdf.format(selectedDate); //преобразуем его под нужные нам значения даты
-            try {
+          /*  selectedDate = sdf.format(selectedDate); //преобразуем его под нужные нам значения даты
+           try {
                 d = sdf.parse(selectedDate);
             } catch (ParseException e) {
                 e.printStackTrace();
-            }
-
-            contentValues.put(DBHelper.KEY_DATATIME, selectedDate);
-            database.insert(DBHelper.TABLE_TRENING, null, contentValues);//записываем в какую таблицу.
-            Toast.makeText(getApplicationContext(), selectedDate+"Данные записаны", Toast.LENGTH_LONG).show();//таск это всплывающие окно, выводим в него наш выбор, заданным форматом
-
-            dbHelper.close();//отключаемся от бд
+            }*/
             dlg.dismiss();//дестрой окна
             Button bt=findViewById(R.id.buttonDateTime);
             bt.setText(selectedDate);
@@ -139,14 +127,34 @@ public void checkBD_Click(View v)//тут проходит проверка бд
             null, null, null);// ТУТ ПРОИСХОДИТ СОРТИРОВКА В ДАЛЬНЕЙШЕМ ПРИГОДИТСЯ ЗАПОМНИ
     if (cursor.moveToFirst()) {
         int idindex = cursor.getColumnIndex(DBHelper.KEY_ID);
+        int nameTrening = cursor.getColumnIndex(DBHelper.KEY_NAMETRENING);
         int dataTime = cursor.getColumnIndex(DBHelper.KEY_DATATIME);
         do {
             Log.d("mLOg ", "ID=" + cursor.getInt(idindex) +
+                    "Имя= " + cursor.getString(nameTrening)+
                     "Дата и время= " + cursor.getString(dataTime));
         } while (cursor.moveToNext());
     }
 }
 
+public void ButtonSavePlane_Click(View v)
+{
+    EditText NameTrening = findViewById(R.id.NameTreningEditText);
+
+    SQLiteDatabase database = dbHelper.getWritableDatabase();//нужен для управления
+    ContentValues contentValues = new ContentValues();//нужен для добавления новых строк в таблицу выглядит как массив с именами столбцов и тд
+
+    contentValues.put(DBHelper.KEY_NAMETRENING, NameTrening.getText().toString());
+    contentValues.put(DBHelper.KEY_DATATIME, selectedDate);
+
+    database.insert(DBHelper.TABLE_TRENING, null, contentValues);//записываем в какую таблицу.
+    Toast.makeText(getApplicationContext(), "Данные записаны", Toast.LENGTH_LONG).show();//таск это всплывающие окно, выводим в него наш выбор, заданным форматом
+
+    dbHelper.close();//отключаемся от бд
+    Intent intent = new Intent(v.getContext(), TreningClass.class);
+    startActivity(intent);
+
+}
 
 }
 
