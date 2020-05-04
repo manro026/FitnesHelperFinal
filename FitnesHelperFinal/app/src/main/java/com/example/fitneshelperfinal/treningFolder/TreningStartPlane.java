2cc97f6983a4622a33eb908fdef2278d69e0e2ca
manro026 <47592702+manro026@users.noreply.github.com>
2020-05-04 17:23:18 +0300

    Сделан поиск по БД, на сегодня хватит

--- FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningStartPlane.java
+++ FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningStartPlane.java
@@ -1,7 +1,10 @@
 package com.example.fitneshelperfinal.treningFolder;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -27,9 +30,10 @@ public class TreningStartPlane extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-
+        dbHelper=new DBHelper(this);
             LayoutInflater inflater = LayoutInflater.from(this);
             List<View> pages = new ArrayList<View>();
+            checkBD();
 
             View page = inflater.inflate(R.layout.test2, null);
 
@@ -51,5 +55,33 @@ public class TreningStartPlane extends Activity {
 
             setContentView(viewPager);
         }
- }
+
+private void checkBD(){
+     String test = "13/5/2020 18:2";
+
+    SQLiteDatabase database = dbHelper.getWritableDatabase();//нужен для управления
+
+    Cursor cursor = database.query(DBHelper.TABLE_TRENING, null, null, null,
+            null, null, null);// ТУТ ПРОИСХОДИТ СОРТИРОВКА В ДАЛЬНЕЙШЕМ ПРИГОДИТСЯ ЗАПОМНИ
+    if (cursor.moveToFirst()) {
+        int idindex = cursor.getColumnIndex(DBHelper.KEY_ID);
+        int nameTrening = cursor.getColumnIndex(DBHelper.KEY_NAMETRENING);
+        int dataTime = cursor.getColumnIndex(DBHelper.KEY_DATATIME);
+
+
+        do {
+            if(test.equals(cursor.getString(dataTime)))
+            {
+                Log.d("mLOg ", "ID=" + cursor.getInt(idindex) +
+                        " Имя= " + cursor.getString(nameTrening)+
+                        " Дата и время= " + cursor.getString(dataTime));
+                break;
+            }
+
+        } while (cursor.moveToNext());
+    }
+
+
+}
+}
 
