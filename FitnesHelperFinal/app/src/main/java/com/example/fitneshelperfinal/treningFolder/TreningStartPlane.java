dec54a2e28d170a6d7d347938cf3e77c95e67339
manro026 <47592702+manro026@users.noreply.github.com>
2020-05-04 16:07:43 +0300

    Убрал скрол, использую метод экранов PageView

--- FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningStartPlane.java
+++ FitnesHelperFinal/app/src/main/java/com/example/fitneshelperfinal/treningFolder/TreningStartPlane.java
@@ -2,14 +2,21 @@ package com.example.fitneshelperfinal.treningFolder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
 import com.example.fitneshelperfinal.MainActivity;
 import com.example.fitneshelperfinal.R;
+import com.example.fitneshelperfinal.SliderPageAdapter;
 import com.example.fitneshelperfinal.bd.DBHelper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TreningStartPlane extends Activity {
 
@@ -22,23 +29,27 @@ public class TreningStartPlane extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
 
-        dbHelper=new DBHelper(this);//оперделим бд
-        main = new MainActivity();//определили его
-     /*   for(int i=0;i<5;i++)
-        {
-            start();
-        }*/
+            LayoutInflater inflater = LayoutInflater.from(this);
+            List<View> pages = new ArrayList<View>();
 
-    }
+            View page = inflater.inflate(R.layout.test2, null);
 
-    private void start()
-    {
-        final LinearLayout testLianer = (LinearLayout) findViewById(R.id.testLianer); // обьявили лайнер который находиться у нас в скроле
-        final View view = getLayoutInflater().inflate(R.layout.test2, null);//выбираем слой какой мы будем туда пихать
-        testLianer.addView(view);
-    }
+            pages.add(page);
 
+            page = inflater.inflate(R.layout.test2, null);
 
+            pages.add(page);
 
+            page = inflater.inflate(R.layout.test2, null);
 
-}
+            pages.add(page);
+
+            SliderPageAdapter pagerAdapter = new SliderPageAdapter(pages);
+            ViewPager viewPager = new ViewPager(this);
+            viewPager.setAdapter(pagerAdapter);
+            viewPager.setCurrentItem(1);
+
+            setContentView(viewPager);
+        }
+ }
+
